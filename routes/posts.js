7635d39bb893f9d4ded1805b94d698ead7ec9c74
stephane510 <60133254+stephane510@users.noreply.github.com>
2024-11-28 16:28:10 +0000

    added post/patch in post file

--- routes/posts.js
+++ routes/posts.js
@@ -9,6 +9,27 @@ const Post = require('../models/Post')
 
 // })
 
+router.post('/',async(req,res)=>{
+
+    const postData = new Post({
+        title:req.body.title,
+        topic:req.body.topic,
+        message:req.body.message,
+        expirationTime:req.body.expirationTime,
+        owner:req.body.owner
+})
+
+    try{
+        const postToSave = await postData.save()
+        res.send(postToSave)
+    }catch(err){
+        res.send({message:err})
+    }
+})
+
+
+
+
 router.get('/', async (req,res) =>{ 
     try{
         const posts = await Post.find() 
@@ -27,5 +48,28 @@ router.get('/:postId', async(req,res)=>{
     }
 
 })
+
+router.patch('/:postId', async(req,res) =>{
+    try{
+        const updatePostById = await Post.updateOne(
+            {_id:req.params.postId},
+            {$set:{
+                title:req.body.title,
+                topic:req.body.topic,
+                message:req.body.message,
+                expirationTime:req.body.expirationTime,
+                owner:req.body.owner
+                }
+            })
+        res.send(updatePostById)
+    }catch(err){
+        res.send({message:err})
+    }
+})
+
+
+
+
+
 
 module.exports = router
